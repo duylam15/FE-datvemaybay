d74b5f97b8b5845228aa99682fcff2045da56ab4
Duy Lam <duylam468213@gmail.com>
2024-12-03 20:52:44 +0700

    a

--- src/pages/admin/dashboard/index.jsx
+++ src/pages/admin/dashboard/index.jsx
@@ -5,7 +5,11 @@ const Dashboard = () => {
     return (
         <>
             <div className='dashboard'>
-                this is a dasshboard page
+                <img src="/public/images/bamboo-airways-anh-1-771.jpg" alt="" />
+                <img src="/public/images/anh-1-1-2136-1660625396-1660631708-1660633105.jpg" alt="" />
+                <img src="/public/images/Bamboo-Airways-ivivu-31.jpg" alt="" />
+                <img src="/public/images/bamboo-airways-ivivu-17.jpg" alt="" />
+                <img src="/public/images/Bamboo.jpg" alt="" />
             </div>
         </>
     );
